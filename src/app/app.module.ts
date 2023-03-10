f366913fd967540dacd52880b41e0cdfbfe13433
sandro sandro <sandroxari@gmail.com>
2023-03-10 23:22:43 +0400

    feature: ngrx devtools added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoEffects } from './todo/store/effects/todo.effects';
 import { TodosDataReducer } from './todo/store/reducers/todo.reducers';
 import { NavbarComponent } from './navbar/navbar.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
@@ -20,6 +22,10 @@ import { NavbarComponent } from './navbar/navbar.component';
       todos: TodosDataReducer,
     }),
     EffectsModule.forRoot([ToDoEffects]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
